52f256699ab20dbaf21098cfe479ad911a6a3f3d
claudi <claudi1618@gmail.com>
2017-12-12 22:26:47 -0700

    Polishing more scripts

---
+++ Boy&BearGame/Assets/Scripts/PlatformReset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformReset : MonoBehaviour {
+
+	bool useGravity;
+	Vector3 originalPosition;
+	void Start(){
+		originalPosition = gameObject.transform.position;
+	}
+	void OnTriggerEnter(Collider touch)
+	{
+		if(touch.gameObject.tag == "PlatformResetFall"){
+		gameObject.GetComponent<Rigidbody>().useGravity = false;	
+		gameObject.GetComponent<Rigidbody>().isKinematic = true;	
+		transform.position = originalPosition;
+		gameObject.SetActive(false);
+		Invoke("PlatformKin", 2f);
+		}
+	}
+		void PlatformKin(){
+		gameObject.SetActive(true);
+		gameObject.GetComponent<Rigidbody>().isKinematic = false;
+	}
+}
