9d1ccc99de94556c3cc271a4958400d34837c448
claudi <claudi1618@gmail.com>
2017-12-14 00:36:35 -0700

    GameText

---
+++ Boy&BearGame/Assets/Scripts/YouWin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouWin : MonoBehaviour {
+	public GameObject youWinText;
+	void Start () {
+		youWinText.SetActive(false);
+	}
+	void OnTriggerEnter(){
+		youWinText.SetActive(true);
+	}
+	
+}
