9552521610d86b5404d0279a24f7a1b19dff4ef2
claudi <claudi1618@gmail.com>
2017-12-13 17:29:57 -0700

    Fixing Textures

--- Boy&BearGame/Assets/Scripts/CloseStart.cs
+++ Boy&BearGame/Assets/Scripts/CloseStart.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CloseStart : MonoBehaviour {
+	public static UnityAction showHUD;
+
 	public void CloseSplash(){
+	showHUD();
 	GameObject.Find("StartScreen").SetActive(false);
 	}
 }
---
+++ Boy&BearGame/Assets/Scripts/HUDShow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDShow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive(false);
+		CloseStart.showHUD += showHUDHandler;
+	}
+	void showHUDHandler(){
+		gameObject.SetActive(true);
+	}
+}
